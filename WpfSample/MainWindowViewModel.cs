f72c57104971e157c458a9c2b7d70c4872db3174
ousttrue <ousttrue@gmail.com>
2015-02-10 02:38:42 +0900

    init

---
+++ WpfSample/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSample
+{
+    class MainWindowViewModel
+    {
+        public String Text { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            Text = "Hello !";
+        }
+    }
+}
