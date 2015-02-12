876cf9e66f5b25e606a6c769743052fdffb5f24b
ousttrue <ousttrue@gmail.com>
2015-02-12 12:45:07 +0900

    save layout

---
+++ AvalonDockSample/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Xceed.Wpf.AvalonDock;
+using Xceed.Wpf.AvalonDock.Layout.Serialization;
+
+namespace AvalonDockSample
+{
+    public class MainWindowViewModel: UriListViewModel.UriListViewModel
+    {
+        String LayoutFile
+        {
+            get
+            {
+                return System.IO.Path.ChangeExtension(
+                    Environment.GetCommandLineArgs()[0]
+                    , ".AvalonDock.config"
+                    );
+            }
+        }
+
+        public void LoadLayout(DockingManager dockManager)
+        {
+            var serializer = new XmlLayoutSerializer(dockManager);
+            try
+            {
+                using (var stream = new StreamReader(LayoutFile))
+                {
+                    serializer.Deserialize(stream);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                ErrorDialog(ex);
+            }
+        }
+
+        public void SaveLayout(DockingManager dockManager)
+        {
+            var serializer = new XmlLayoutSerializer(dockManager);
+            using (var stream = new StreamWriter(LayoutFile))
+            {
+                serializer.Serialize(stream);
+            }
+        }
+    }
+}
